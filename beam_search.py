04ebeae7f1bb4d10d0ad04fbceb723db58074e02
Nghi Bui <bdqnghi@gmail.com>
2019-07-18 01:43:20 +0800

    update

--- beam_search.py
+++ beam_search.py
@@ -20,59 +20,6 @@ import operator
 import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-
-def generate_graph_file(test_file_path):
-    fbs_path = test_file_path.split(".")[0] + ".fbs"
-    graph_path = test_file_path.split(".")[0] + ".txt"
-
-    if not os.path.exists(fbs_path):
-
-        fbs_cmd = "docker run --rm -v $(pwd):/e -it yijun/fast -S -G " + test_file_path + " " + fbs_path
-        os.system(fbs_cmd)
-
-    if not os.path.exists(graph_path):
-        ggnn_cmd = "docker run -v $(pwd):/e --entrypoint ggnn -it yijun/fast " + fbs_path + " " + test_file_path.split(".")[0] + "_train.txt" + " " + graph_path
-        print(ggnn_cmd)
-        os.system(ggnn_cmd)
-    
-    
-    return graph_path
-
-def generate_statements_file(pb_path, statement_type, output_path):
-    
-    if not os.path.exists(output_path):
-        generate_stmt_cmd = "docker run --rm -v $(pwd):/e -it yijun/fast --node_types=" + statement_type + " " + pb_path + " " + "temp.pb" + " > " + output_path
-        print(generate_stmt_cmd)
-        os.system(generate_stmt_cmd)
-    # return stmt_ids_path
-
-def generate_pb_file(test_file_path):
-    print("Generating pb with src_path : " + test_file_path)
-    pb_path = os.path.join(test_file_path.split(".")[0] + ".pb")
-    if not os.path.exists(pb_path):
-        cmd = "docker run --rm -v $(pwd):/e -it yijun/fast -p " + test_file_path + " " + pb_path
-        os.system(cmd)
-    return pb_path
-
-def generate_subtrees(pb_path, stmt_ids):
-
-    subtrees_dict = {}
-    for stmt_id in stmt_ids:
-        subtree_ids_path =   os.path.join(pb_path.split(".")[0] + "_subtree_" + str(stmt_id) + ".csv")
-        generate_subtree_ids_cmd = "docker run -v $(pwd):/e yijun/fast -CA " +  str(stmt_id) + " " + pb_path + " > " + subtree_ids_path        
-        os.system(generate_subtree_ids_cmd)
-        
-        subtree_ids = []
-        with open(subtree_ids_path,"r") as f1:
-            data = f1.readlines()
-            for line in data:
-                subtree_id = int(line.replace("\n",""))
-                subtree_ids.append(subtree_id)
-
-        subtrees_dict[int(stmt_id)] = subtree_ids
-
-    return subtrees_dict
-            
 def fetch_data_from_github(filename):
     if not os.path.exists(os.path.dirname(filename)):
         os.makedirs(os.path.dirname(filename))
@@ -84,96 +31,10 @@ def fetch_data_from_github(filename):
     curl.close()
     fp.close()   
 
-def generate_statement_ids(pb_path):
-    # Create model path folder if not exists
+
+def beam_search():
     
-    # generate_graph_files(opt)
-    # print(stmt_ids_path)
-
-    single_stmt_ids_path = os.path.join(pb_path.split(".")[0] + "_expr_stmt_ids.txt")
-    generate_statements_file(pb_path, "statements_single.csv",single_stmt_ids_path)
-
-    condition_stmt_ids_path = os.path.join(pb_path.split(".")[0] + "_condition_stmt_ids.txt")
-    generate_statements_file(pb_path, "statements_condition.csv",condition_stmt_ids_path)
-
-    all_stmt_ids_path = os.path.join(pb_path.split(".")[0] + "_stmt_ids.txt")
-    if os.path.exists(all_stmt_ids_path):
-        os.remove(all_stmt_ids_path)
-
-    stmt_ids = []
-    with open(single_stmt_ids_path,"r") as f:
-        data = f.readlines()
-        for line in data:
-            stmt_ids.append(line.replace("\n",""))
-
-    with open(condition_stmt_ids_path,"r") as f1:
-        data = f1.readlines()
-        for line in data:
-            stmt_ids.append(line.replace("\n",""))
-
-    with open(all_stmt_ids_path,"w") as f2:
-        for stmt_id in stmt_ids:
-            f2.write(str(stmt_id))
-            f2.write("\n")
-        
-    return stmt_ids
-
-def scale_attention_scores(attention_scores_map):
-    attention_scores_sorted = sorted(attention_scores_map.items(), key=operator.itemgetter(1))
-    attention_scores_sorted.reverse()
-
-    node_ids = []
-    attention_scores = []
-    raw_attention_scores_dict = {}
-    for element in attention_scores_sorted:
-        node_ids.append(element[0])
-        attention_scores.append(element[1])
-        raw_attention_scores_dict[element[0]] = element[1]
-    scaled_attention_scores = scale_attention_score_by_group(attention_scores)
-
-    scaled_attention_scores_dict = {}
-    for i, score in enumerate(scaled_attention_scores):
-        key = str(node_ids[i])
-        scaled_attention_scores_dict[key] = float(score)
-
-    return scaled_attention_scores_dict, raw_attention_scores_dict
-
-def generate_attention_scores(test_file_path, attention_scores):
-    attention_scores_map = {}
-    for i, score in enumerate(attention_scores):
-        attention_scores_map[i] = float(score)
-
-
-    scaled_attention_scores_dict, raw_attention_scores_dict = scale_attention_scores(attention_scores_map)
-
-    scaled_attention_scores_path = os.path.join(test_file_path.split(".")[0] + "_scaled.csv")
-
-    with open(scaled_attention_scores_path,"w") as f:
-        for k, v in scaled_attention_scores_dict.items():
-            f.write(str(k) + "," + str(v))
-            f.write("\n")
-
-    raw_attention_scores_path = os.path.join(test_file_path.split(".")[0] + "_raw.csv")
-
-    with open(raw_attention_scores_path,"w") as f:
-        for k, v in raw_attention_scores_dict.items():
-            f.write(str(k) + "," + str(v))
-            f.write("\n")
-     
-    return scaled_attention_scores_path, raw_attention_scores_path, raw_attention_scores_dict
-
-def compute_components_attention_score(subtrees_dict, raw_attention_scores_dict):
-    # print(subtrees_dict)
-    # print(raw_attention_scores_dict)
-    component_scores_dict = {}
-    for component_id, subtree_ids in subtrees_dict.items():
-        component_scores_dict[component_id] = 0
-        for subtree_id in subtree_ids:
-            component_scores_dict[component_id] += raw_attention_scores_dict[subtree_id]
-    return component_scores_dict
-
-
-
+    
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
@@ -215,10 +76,6 @@ def main():
     opt.test_graph_path = generate_graph_file(opt.test_file)
     opt.pb_path = generate_pb_file(opt.test_file)
 
-    # Generate statement id and subtree ids
-    stmt_ids = generate_statement_ids(opt.pb_path)
-    subtrees_dict = generate_subtrees(opt.pb_path, stmt_ids)
-
     # Init the model
     checkfile = os.path.join(opt.model_path, 'cnn_tree.ckpt')    
     ckpt = tf.train.get_checkpoint_state(opt.model_path)
@@ -239,16 +96,12 @@ def main():
             for i, var in enumerate(saver._var_list):
                 print('Var {}: {}'.format(i, var))
 
-            prediction_results = making_prediction(test_dataset, ggnn, sess, opt, True)
             
-            component_attention_scores_dict = compute_components_attention_score(subtrees_dict, prediction_results["raw_attention_scores_dict"])
-            
-            components_attention_scores_path = os.path.join(opt.pb_path.split(".")[0] + "_component_ids.csv")
+            components_attention_scores_path = os.path.join(opt.test_file.split(".")[0] + "_component_ids.csv")
 
-            with open(components_attention_scores_path,"w") as f:
-                for k, v in component_attention_scores_dict.items():
-                    f.write(str(k) + "," + str(v))
-                    f.write("\n")
+            component_attention_scores_dict = {}
+            with open(components_attention_scores_path,"r") as f:
+                
 
 def making_prediction(test_dataset, ggnn, sess, opt, original=False):
 
