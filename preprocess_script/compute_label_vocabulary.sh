10c858863741a94ea5cafa789d72c46e9e85b099
Nghi Bui <bdqnghi@gmail.com>
2019-12-19 18:17:26 +0000

    update

--- preprocess_script/compute_label_vocabulary.sh
+++ preprocess_script/compute_label_vocabulary.sh
@@ -1,11 +1,11 @@
-TRAIN_DIR=../sample_data/java-small-graph/training
-TEST_DIR=../sample_data/java-small-graph/test
-VAL_DIR=../sample_data/java-small-graph/validation
+TRAIN_DIR=../code2vec_data/java-small-graph/training
+TEST_DIR=../code2vec_data/java-small-graph/test
+VAL_DIR=../code2vec_data/java-small-graph/validation
 TRAIN_LABEL_VOCAB_PATH=../preprocessed_data/train_label_vocab.txt
 TEST_LABEL_VOCAB_PATH=../preprocessed_data/test_label_vocab.txt
 VALIDATION_LABEL_VOCAB_PATH=../preprocessed_data/val_label_vocab.txt
 PYTHON=python3
 
 ${PYTHON} compute_label_vocabulary.py --input ${TRAIN_DIR} --output ${TRAIN_LABEL_VOCAB_PATH} 
-${PYTHON} compute_label_vocabulary.py --input ${TRAIN_DIR} --output ${TEST_LABEL_VOCAB_PATH}
+${PYTHON} compute_label_vocabulary.py --input ${TEST_DIR} --output ${TEST_LABEL_VOCAB_PATH}
 ${PYTHON} compute_label_vocabulary.py --input ${VAL_DIR} --output ${VALIDATION_LABEL_VOCAB_PATH}
--- preprocess_script/compute_token_vocabulary.sh
+++ preprocess_script/compute_token_vocabulary.sh
@@ -1,11 +1,11 @@
-TRAIN_DIR=../sample_data/java-small-graph/training
-TEST_DIR=../sample_data/java-small-graph/test
-VAL_DIR=../sample_data/java-small-graph/validation
+TRAIN_DIR=../code2vec_data/java-small-graph/training
+TEST_DIR=../code2vec_data/java-small-graph/test
+VAL_DIR=../code2vec_data/java-small-graph/validation
 TRAIN_TOKEN_VOCAB_PATH=../preprocessed_data/train_token_vocab.txt
 TEST_TOKEN_VOCAB_PATH=../preprocessed_data/test_token_vocab.txt
 VALIDATION_TOKEN_VOCAB_PATH=../preprocessed_data/val_token_vocab.txt
 PYTHON=python3
 
 ${PYTHON} compute_token_vocabulary.py --input ${TRAIN_DIR} --output ${TRAIN_TOKEN_VOCAB_PATH} 
-${PYTHON} compute_token_vocabulary.py --input ${TRAIN_DIR} --output ${TEST_TOKEN_VOCAB_PATH}
+${PYTHON} compute_token_vocabulary.py --input ${TEST_DIR} --output ${TEST_TOKEN_VOCAB_PATH}
 ${PYTHON} compute_token_vocabulary.py --input ${VAL_DIR} --output ${VALIDATION_TOKEN_VOCAB_PATH}
