536dc479cfcc96cff9fde6a6fbfcb148b75b274a
Quoc Nghi <bdqnghi@gmail.com>
2020-01-24 01:06:59 +0800

    test script

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-import numpy as np
-def _onehot(i, total):
-    zeros = np.zeros(total)
-    zeros[i] = 1.0
-    return zeros
 
-num_labels = 200000
-# label_lookup_onehot = {label: _onehot(label, num_labels) for label in range(0, num_labels)}
-# print(label_lookup_onehot)
-labels = [] 
-for label in range(0, num_labels):
-    label = _onehot(label, num_labels)
-    labels.append(label)
+import random
+#import pickle
+import pyarrow
+
+saved_input_filename = "code2vec_data/java-small-graph-transformed/java-small-graph-transformed-training.pkl"
+
+input_file = open(saved_input_filename, 'rb')
+buf = input_file.read()
+buckets, bucket_sizes, bucket_at_step = pyarrow.deserialize(buf)
+input_file.close()
+
+for bucket_idx, bucket_data in buckets.items():
+    print("-------------")
+    print("Bucket id : " + str(bucket_idx))
+    print("Num items : " + str(len(bucket_data)))
