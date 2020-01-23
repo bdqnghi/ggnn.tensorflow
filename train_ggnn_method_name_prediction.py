51ea611b542d38d8f74dcb27abe7f7b1bb3446f8
Quoc Nghi <bdqnghi@gmail.com>
2020-01-24 03:36:28 +0800

    update

--- train_ggnn_method_name_prediction.py
+++ train_ggnn_method_name_prediction.py
@@ -57,7 +57,7 @@ parser.add_argument('--n_hidden', type=int, default=50,
 parser.add_argument('--log_path', default="logs/",
                     help='log path for tensorboard')
 parser.add_argument('--checkpoint_every', type=int,
-                    default=10000, help='check point to save model')
+                    default=500, help='check point to save model')
 parser.add_argument('--best_f1', type=float,
                     default=0.0, help='best f1 to save model')
 parser.add_argument('--aggregation', type=int, default=3, choices=range(0, 4),
