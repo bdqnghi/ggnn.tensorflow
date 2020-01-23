70b0c66cec354548f1fdb03d65bc29770a13c465
Quoc Nghi <bdqnghi@gmail.com>
2020-01-24 01:11:33 +0800

    test script

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import random
 #import pickle
 import pyarrow
 
-saved_input_filename = "code2vec_data/java-small-graph-transformed/java-small-graph-transformed-training.pkl"
+saved_input_filename = "sample_data/java-small-graph-transformed/java-small-graph-transformed-training.pkl"
 
 input_file = open(saved_input_filename, 'rb')
 buf = input_file.read()
@@ -13,4 +13,5 @@ input_file.close()
 for bucket_idx, bucket_data in buckets.items():
     print("-------------")
     print("Bucket id : " + str(bucket_idx))
+    print("Bucket size : " + str(bucket_sizes[bucket_idx]))
     print("Num items : " + str(len(bucket_data)))
