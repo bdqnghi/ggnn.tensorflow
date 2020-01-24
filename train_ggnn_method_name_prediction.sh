2d5862d24dc45230b5a625b16c15a1d03d6aaa24
Nghi Bui <bdqnghi@gmail.com>
2020-01-24 13:15:59 +0800

    update

--- train_ggnn_method_name_prediction.sh
+++ train_ggnn_method_name_prediction.sh
@@ -5,5 +5,6 @@ BATCH_SIZE=20
 VAL_BATCH_SIZE=3
 DATA_THRESHOLD=100000
 BUCKET_SIZE_THRESHOLD=1000
+CHECKPOINT_EVERY=1000
 PYTHON=python3
-${PYTHON} train_ggnn_method_name_prediction.py --train_path ${TRAIN_PATH} --val_path ${VAL_PATH} --batch_size ${BATCH_SIZE} --val_batch_size ${VAL_BATCH_SIZE} --data_threshold ${DATA_THRESHOLD} --bucket_size_threshold ${BUCKET_SIZE_THRESHOLD}
+${PYTHON} train_ggnn_method_name_prediction.py --train_path ${TRAIN_PATH} --val_path ${VAL_PATH} --batch_size ${BATCH_SIZE} --val_batch_size ${VAL_BATCH_SIZE} --data_threshold ${DATA_THRESHOLD} --bucket_size_threshold ${BUCKET_SIZE_THRESHOLD} --checkpoint_every ${CHECKPOINT_EVERY}
