f9b88817ba330e6142939b00302eee6b58da4cf8
Nghi Bui <bdqnghi@gmail.com>
2020-01-24 15:35:37 +0800

    add validating condition

--- train_ggnn_method_name_prediction.sh
+++ train_ggnn_method_name_prediction.sh
@@ -6,5 +6,7 @@ VAL_BATCH_SIZE=3
 DATA_THRESHOLD=100000
 BUCKET_SIZE_THRESHOLD=1000
 CHECKPOINT_EVERY=1000
+CUDA=0
+VALIDATING=1
 PYTHON=python3
-${PYTHON} train_ggnn_method_name_prediction.py --train_path ${TRAIN_PATH} --val_path ${VAL_PATH} --batch_size ${BATCH_SIZE} --val_batch_size ${VAL_BATCH_SIZE} --data_threshold ${DATA_THRESHOLD} --bucket_size_threshold ${BUCKET_SIZE_THRESHOLD} --checkpoint_every ${CHECKPOINT_EVERY}
+${PYTHON} train_ggnn_method_name_prediction.py --train_path ${TRAIN_PATH} --val_path ${VAL_PATH} --batch_size ${BATCH_SIZE} --val_batch_size ${VAL_BATCH_SIZE} --data_threshold ${DATA_THRESHOLD} --bucket_size_threshold ${BUCKET_SIZE_THRESHOLD} --checkpoint_every ${CHECKPOINT_EVERY} --cuda ${CUDA} --validating ${VALIDATING}
