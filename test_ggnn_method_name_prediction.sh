523f0415ed1842ed3a10ffe6c43dd84c9829cd75
Quoc Nghi <bdqnghi@gmail.com>
2020-01-26 04:52:40 +0800

    update params

--- test_ggnn_method_name_prediction.sh
+++ test_ggnn_method_name_prediction.sh
@@ -4,7 +4,8 @@ BATCH_SIZE=32
 VAL_BATCH_SIZE=3
 SAMPLING_SIZE=1
 GRAPH_SIZE_THRESHOLD=1500
-CUDA=0
-VALIDATING=1
+CUDA=-1
+NODE_TYPE_DIM=30
+NODE_TOKEN_DIM=50
 PYTHON=python3
-${PYTHON} test_ggnn_method_name_prediction.py --val_path ${VAL_PATH} --batch_size ${BATCH_SIZE} --val_batch_size ${VAL_BATCH_SIZE} --sampling_size ${SAMPLING_SIZE} --cuda ${CUDA} --validating ${VALIDATING} --graph_size_threshold ${GRAPH_SIZE_THRESHOLD}
+${PYTHON} test_ggnn_method_name_prediction.py --val_path ${VAL_PATH} --batch_size ${BATCH_SIZE} --val_batch_size ${VAL_BATCH_SIZE} --sampling_size ${SAMPLING_SIZE} --cuda ${CUDA} --validating ${VALIDATING} --graph_size_threshold ${GRAPH_SIZE_THRESHOLD} --node_type_dim ${NODE_TYPE_DIM} --node_token_dim ${NODE_TOKEN_DIM}
--- train_ggnn_method_name_prediction.sh
+++ train_ggnn_method_name_prediction.sh
@@ -8,5 +8,7 @@ CHECKPOINT_EVERY=1000
 GRAPH_SIZE_THRESHOLD=1500
 CUDA=0
 VALIDATING=1
+NODE_TYPE_DIM=30
+NODE_TOKEN_DIM=50
 PYTHON=python3
-${PYTHON} train_ggnn_method_name_prediction.py --train_path ${TRAIN_PATH} --val_path ${VAL_PATH} --batch_size ${BATCH_SIZE} --val_batch_size ${VAL_BATCH_SIZE} --sampling_size ${SAMPLING_SIZE} --checkpoint_every ${CHECKPOINT_EVERY} --cuda ${CUDA} --validating ${VALIDATING} --graph_size_threshold ${GRAPH_SIZE_THRESHOLD}
+${PYTHON} train_ggnn_method_name_prediction.py --train_path ${TRAIN_PATH} --val_path ${VAL_PATH} --batch_size ${BATCH_SIZE} --val_batch_size ${VAL_BATCH_SIZE} --sampling_size ${SAMPLING_SIZE} --checkpoint_every ${CHECKPOINT_EVERY} --cuda ${CUDA} --validating ${VALIDATING} --graph_size_threshold ${GRAPH_SIZE_THRESHOLD} --node_type_dim ${NODE_TYPE_DIM} --node_token_dim ${NODE_TOKEN_DIM}
