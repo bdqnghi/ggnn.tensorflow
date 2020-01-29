5a6a3eefc88de5a6ef74ae77e2b31960baec7b48
Nghi Bui <bdqnghi@gmail.com>
2020-01-29 17:22:35 +0800

    add dataset param

--- test_ggnn_method_name_prediction.sh
+++ test_ggnn_method_name_prediction.sh
@@ -14,6 +14,7 @@ NODE_TYPE_DIM=30
 NODE_TOKEN_DIM=50
 PYTHON=python3
 ${PYTHON} test_ggnn_method_name_prediction.py --val_path ${VAL_PATH} --batch_size ${BATCH_SIZE} \
+--dataset ${DATASET} \
 --val_batch_size ${VAL_BATCH_SIZE} --sampling_size ${SAMPLING_SIZE} --cuda ${CUDA} --node_type_dim ${NODE_TYPE_DIM} \
 --node_token_dim ${NODE_TOKEN_DIM} \
 --node_type_dim ${NODE_TYPE_DIM} --node_token_dim ${NODE_TOKEN_DIM} \
--- train_ggnn_method_name_prediction.sh
+++ train_ggnn_method_name_prediction.sh
@@ -11,13 +11,14 @@ BATCH_SIZE=12
 VAL_BATCH_SIZE=10
 SAMPLING_SIZE=1
 CHECKPOINT_EVERY=300
-GRAPH_SIZE_THRESHOLD=1500
+GRAPH_SIZE_THRESHOLD_LOWER=1500
 CUDA=0
 VALIDATING=1
 NODE_TYPE_DIM=30
 NODE_TOKEN_DIM=50
 PYTHON=python3
 ${PYTHON} train_ggnn_method_name_prediction.py \
+--dataset ${DATASET} \
 --train_path ${TRAIN_PATH} --val_path ${VAL_PATH} --batch_size ${BATCH_SIZE} \
 --val_batch_size ${VAL_BATCH_SIZE} --sampling_size ${SAMPLING_SIZE} --checkpoint_every \
 ${CHECKPOINT_EVERY} --cuda ${CUDA} --validating ${VALIDATING} --graph_size_threshold ${GRAPH_SIZE_THRESHOLD} \
