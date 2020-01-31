23b28861f7dd6029b95c209710b0c960f5e607e8
Quoc Nghi <bdqnghi@gmail.com>
2020-01-31 19:26:28 +0800

    add params to data processing_script

--- data_processing.sh
+++ data_processing.sh
@@ -4,8 +4,10 @@ TRAIN_DIR=../${DATA}/${DATASET}/training
 TEST_DIR=../${DATA}/${DATASET}/test
 VAL_DIR=../${DATA}/${DATASET}/validation
 NUM_WORKER=10
+FBS=1
+TXT=0
 PYTHON=python3
 
-${PYTHON} data_processing.py --path ${TRAIN_DIR} --worker ${NUM_WORKER}
-${PYTHON} data_processing.py --path ${TEST_DIR} --worker ${NUM_WORKER}
-${PYTHON} data_processing.py --path ${VAL_DIR} --worker ${NUM_WORKER}
+${PYTHON} data_processing.py --path ${TRAIN_DIR} --worker ${NUM_WORKER} --fbs ${FBS} --txt ${TXT}
+${PYTHON} data_processing.py --path ${TEST_DIR} --worker ${NUM_WORKER} --fbs ${FBS} --txt ${TXT}
+${PYTHON} data_processing.py --path ${VAL_DIR} --worker ${NUM_WORKER} --fbs ${FBS} --txt ${TXT}
