2d69db2dcb4d77ba951f51968d3b564584678902
Quoc Nghi <bdqnghi@gmail.com>
2020-02-06 05:01:41 +0800

    update

--- preprocess_script/compute_label_vocabulary.py
+++ preprocess_script/compute_label_vocabulary.py
@@ -61,17 +61,18 @@ def main():
 				if splits[0] == "?":
 					file_path_splits = splits[1].split("/")
 					file_name = file_path_splits[len(file_path_splits)-1]
-					method_name = file_name.split("_")[1]
-					try:
-						method_name = file_name.split("_")[1].replace(".java","")
-					
-					except Exception as e:
-						print(e)
-						with open("error.txt", "w") as f3:
-							f3.write(method_name + " " + str(e))
+					if "_" in file_name:
+						method_name = file_name.split("_")[1]
+						try:
+							method_name = file_name.split("_")[1].replace(".java","")
+						
+						except Exception as e:
+							print(e)
+							with open("error.txt", "w") as f3:
+								f3.write(file_name + " " + str(e))
 
-					if method_name not in excluded_tokens:
-						all_vocabularies.add(method_name)
+						if method_name not in excluded_tokens:
+							all_vocabularies.add(method_name)
 					
 
 	# all_vocabularies = exclude_tokens(all_vocabularies)
