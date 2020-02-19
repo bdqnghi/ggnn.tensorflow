d99898d23b4603bdf60148540368502ded74da23
Quoc Nghi <bdqnghi@gmail.com>
2020-02-20 04:43:30 +0800

    update

--- train_treecaps_method_name_prediction.sh
+++ train_treecaps_method_name_prediction.sh
@@ -7,7 +7,7 @@ NODE_TYPE_VOCABULARY_PATH=preprocessed_data/node_type_vocab.txt
 TOKEN_VOCABULARY_PATH=preprocessed_data/treecaps/${DATASET}/token_vocab.txt
 TRAIN_LABEL_VOCABULARY_PATH=preprocessed_data/treecaps/${DATASET}/train_label_vocab.txt
 VAL_LABEL_VOCABULARY_PATH=preprocessed_data/treecaps/${DATASET}/val_label_vocab.txt
-BATCH_SIZE=10
+BATCH_SIZE=1
 VAL_BATCH_SIZE=32
 SAMPLING_SIZE=1
 CHECKPOINT_EVERY=10
@@ -19,9 +19,10 @@ NODE_TOKEN_DIM=50
 TOP_A=10
 TOP_B=15
 NUM_CONV=8
-OUTPUT_SIZE=16
+OUTPUT_SIZE=128
 CAPS1_NUM_DIMS=8
 CAPS1_OUT_DIMS=8
+NUM_FILES_THRESHOLD=20000
 TASK=1
 PYTHON=python3
 ${PYTHON} train_treecaps_method_name_prediction.py \
@@ -36,4 +37,4 @@ ${CHECKPOINT_EVERY} --cuda ${CUDA} --validating ${VALIDATING} --tree_size_thresh
 --val_label_vocabulary_path ${VAL_LABEL_VOCABULARY_PATH} \
 --task ${TASK} \
 --top_a ${TOP_A} --top_b ${TOP_B} --num_conv ${NUM_CONV} --output_size ${OUTPUT_SIZE} \
---caps1_num_dims ${CAPS1_NUM_DIMS} --caps1_out_dims ${CAPS1_OUT_DIMS}
+--caps1_num_dims ${CAPS1_NUM_DIMS} --caps1_out_dims ${CAPS1_OUT_DIMS} --num_files_threshold ${NUM_FILES_THRESHOLD}
