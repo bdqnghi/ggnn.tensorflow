9e5b8d82b3c07ead3fdf6f167cd091a654d4b6f2
Nghi Bui <bdqnghi@gmail.com>
2020-02-23 03:07:40 +0800

    update dataprocessing for transformed part

--- data_processing_for_transformed_data.sh
+++ data_processing_for_transformed_data.sh
@@ -1,15 +1,12 @@
-DATA=sample_data
-DATASET=java-small
-TRAIN_DIR=${DATA}/${DATASET}/training
-TEST_DIR=${DATA}/${DATASET}/test
-VAL_DIR=${DATA}/${DATASET}/validation
+INPUT_PATH=transformed_data
+DATASET=java-med
+PART=testing
 NUM_WORKER=16
 FBS=0
 TXT=1
 PKL=0
 PB=0
 PYTHON=python3
-
-${PYTHON} data_processing.py --path ${TRAIN_DIR} --worker ${NUM_WORKER} --fbs ${FBS} --txt ${TXT} --pkl ${PKL} --pb ${PB}
+${PYTHON} data_processing.py --path ${INPUT_PATH} --part ${PART} --dataset ${DATASET} --worker ${NUM_WORKER} --fbs ${FBS} --txt ${TXT} --pkl ${PKL} --pb ${PB}
 #${PYTHON} data_processing.py --path ${TEST_DIR} --worker ${NUM_WORKER} --fbs ${FBS} --txt ${TXT} --pkl ${PKL} --pb ${PB}
 #${PYTHON} data_processing.py --path ${VAL_DIR} --worker ${NUM_WORKER} --fbs ${FBS} --txt ${TXT} --pkl ${PKL} --pb ${PB}
