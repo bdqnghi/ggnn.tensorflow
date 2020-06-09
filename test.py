0a05abc6f4ff8260c7c3aa097ca6d226554696cc
Quoc Nghi <bdqnghi@gmail.com>
2020-06-09 14:54:14 +0800

    CLEAN UP

--- test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-x = np.random.rand(3,2)
-y = np.random.rand(1,3,2)
-
-z = x + y
-print(z.shape)
--- test_gpu.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-tf.test.is_gpu_available(
-    cuda_only=False,
-    min_cuda_compute_capability=None
-)
