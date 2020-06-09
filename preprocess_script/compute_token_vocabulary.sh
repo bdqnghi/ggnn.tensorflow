526bc78d8b3a12f04f87cd9e594ca6dacced848d
Quoc Nghi <bdqnghi@gmail.com>
2020-06-09 14:53:38 +0800

    update

--- preprocess_script/compute_token_vocabulary.sh
+++
@@ -1,8 +0,0 @@
-DATA=sample_data
-DATASET=java-small
-DATASET_GRAPH=${DATASET}-graph
-MODEL=treecaps
-TRAIN_DIR=../${DATA}/${DATASET_GRAPH}/training
-TOKEN_VOCAB_PATH=../preprocessed_data/${MODEL}/${DATASET}/token_vocab.txt
-PYTHON=python3
-${PYTHON} compute_token_vocabulary.py --input ${TRAIN_DIR} --output ${TOKEN_VOCAB_PATH} 
--- preprocess_script/data_preparation.sh
+++
@@ -1,3 +0,0 @@
-source compute_label_vocabulary.sh
-source compute_token_vocabulary.sh
-source transform_graph.sh
